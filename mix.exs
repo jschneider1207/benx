6b83ac31b3d4f9d093949e19704c7b8d88223a95
Jessica Schneider <jessica.schneider1207@gmail.com>
2016-12-26 14:15:07 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Benx.Mixfile do
 
   def project do
     [app: :benx,
-     version: "0.1.0",
+     version: "0.1.1",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
